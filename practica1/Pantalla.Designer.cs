c64ae050632f94f3c1319d2f8c71286cda98439b
PAULO <adrian.paulo.vaz@gmail.com>
2024-09-25 14:57:04 -0600

    create repo

---
+++ practica1/Pantalla.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace practica1
+{
+    partial class Pantalla
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn = new System.Windows.Forms.Button();
+            this.lbl = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lblsaludo = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lblnombre = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn
+            // 
+            this.btn.Font = new System.Drawing.Font("Monaspace Argon SemiWide ExtraB", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn.Location = new System.Drawing.Point(157, 323);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(284, 78);
+            this.btn.TabIndex = 0;
+            this.btn.Text = "Borrar System32";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // lbl
+            // 
+            this.lbl.AutoSize = true;
+            this.lbl.Font = new System.Drawing.Font("Monaspace Neon", 10.8F, System.Drawing.FontStyle.Bold);
+            this.lbl.Location = new System.Drawing.Point(395, 262);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(153, 22);
+            this.lbl.TabIndex = 2;
+            this.lbl.Text = "Presione para";
+            this.lbl.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Monaspace Argon ExtraBold", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(493, 323);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(284, 78);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Restaurar System32";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(157, 53);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 22);
+            this.txtNombre.TabIndex = 4;
+            // 
+            // lblsaludo
+            // 
+            this.lblsaludo.AutoSize = true;
+            this.lblsaludo.Font = new System.Drawing.Font("Monaspace Argon", 12F);
+            this.lblsaludo.Location = new System.Drawing.Point(471, 85);
+            this.lblsaludo.Name = "lblsaludo";
+            this.lblsaludo.Size = new System.Drawing.Size(82, 24);
+            this.lblsaludo.TabIndex = 5;
+            this.lblsaludo.Text = "Nombre";
+            this.lblsaludo.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(157, 103);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "click aquí";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lblnombre
+            // 
+            this.lblnombre.AutoSize = true;
+            this.lblnombre.Location = new System.Drawing.Point(80, 53);
+            this.lblnombre.Name = "lblnombre";
+            this.lblnombre.Size = new System.Drawing.Size(56, 16);
+            this.lblnombre.TabIndex = 7;
+            this.lblnombre.Text = "Nombre";
+            // 
+            // Pantalla
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblnombre);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lblsaludo);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lbl);
+            this.Controls.Add(this.btn);
+            this.Name = "Pantalla";
+            this.ShowIcon = false;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.Label lbl;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label lblsaludo;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lblnombre;
+    }
+}
+
---
+++ practica1/Pantalla.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practica1
+{
+    public partial class Pantalla : Form
+    {
+        public Pantalla()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Código que se ejecuta al cargar el formulario
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            // Evento al hacer clic en label2
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (lbl.Text == "Borrando System32")
+            {
+                lbl.Text = "System32 Restaurado";
+            }
+            else
+            {
+                MessageBox.Show("System32 no existe", "Mensaje");
+            }
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            if (lbl.Text == "System32 Restaurado")
+            {
+                lbl.Text = "Borrando System32";
+            }
+            else
+            {
+                MessageBox.Show("System32 está en su lugar.", "Mensaje");
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            lblsaludo.Text = "Hola "+ txtNombre.Text;
+        }
+    }
+}
